0cc33024ae6b0fa247f3922ef0729ddcc610aada
Xavalicious <xemonkey@live.com>
2016-10-11 12:29:13 -0600

    done

---
+++ FullName/CircleRadii.java
@@ -0,0 +1,77 @@
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+public class CircleRadii {
+
+	public static void main(String[] args) {
+		/*int m=97, k=0, p=0;
+		while (m <= 195)
+		{
+		k = k * k + 3 * m;
+		p = p + m + 1;
+		m++;
+		}
+		System.out.println(m+" "+p+" "+k);*/
+		
+		/*int m, k=0, p=0;
+		for (m = 97; m <= 195; m++)
+		{
+		k = k * k + 3 * m;
+		p = p + m + 1;
+		}
+		System.out.println(k+" "+p+" "+m);*/
+		
+		/*int k=0, q=1;
+		for(int v = 2; v <= 195; v *= 3)
+		{
+		k = k * k + 3 * v;
+		q = Math.sqrt(q + v + 1);*/
+		
+		/*int i = 0, j = 0;
+		while (i <= 3)
+		{
+			for (j = 0; j<=2; j++)
+			{
+				System.out.print(i + "," + j + " ");
+			}
+			i++;*/
+		
+		/*double m=92.801;
+		int j = 0;
+		do
+		{
+			j = j + 2;
+			if (j > -100)
+				continue;
+			m+=3;
+		} while (j < 6);
+		System.out.println(m);*/
+		
+		int a;
+		do
+		{Scanner IntTaker=new Scanner(System.in);
+		System.out.println("Enter an integer here");
+		a=IntTaker.nextInt();
+		System.out.println(Math.pow(a, 2));
+		} while (a!=0);
+		
+		
+		
+		
+		
+	
+		
+	}
+	
+	
+	
+	
+	}
+
+	
+
+
+	
+
+
---
+++ FullName/FullNameTest.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class FullNameTest {
+
+	public static void main(String[] args) {
+		
+			Scanner XavyWavy=new Scanner(System.in);
+			System.out.println("What is your first name? ");
+			String first=XavyWavy.nextLine();
+			
+			Scanner AdrianLikesMusic=new Scanner(System.in);
+			System.out.println("What is your last name? ");
+			String last=AdrianLikesMusic.nextLine();
+			
+			System.out.println("Your full name is " + first + " " + last + ".");
+		}
+
+	
+
+	}
+
+
